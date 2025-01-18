cc0d894ceefcd2f781a9f8e9c7cc8e4df13036d5
Tejas Satish Navalkhe <navalkhetejas18@gmail.com>
2025-01-18 17:53:02 +0530

    notify me email added

--- tejasnavalkhe/main/utils.py
+++ tejasnavalkhe/main/utils.py
@@ -3,15 +3,16 @@ from tejasnavalkhe import mail
 from flask_mail import Message
 
 def new_subscription(email):
-	msg = Message('Thank You for Subscribing!', sender=(Config.MAIL_NAME, Config.MAIL_USERNAME), recipients=email)
-	msg.body = '''Hi,
+	msg = Message('Thank You for Subscribing!', sender=(Config.MAIL_NAME, Config.MAIL_USERNAME), recipients=[email])
+	msg.body = '''
+	Hi,
 
 	Thank you for signing up at tejasnavalkhe.com! I'm excited to have you on board.
 
 	You'll be among the first to receive updates, insights, and exclusive announcements as things progress. Stay tuned—there's a lot to look forward to! 
 	Stay tuned for more details—exciting things are on the horizon!
 
-	In the meantime, feel free to connect with me or share any thoughts by replying to this email.
+	In the meantime, feel free to connect with me or share any thoughts at contact@tejasnavalkhe.com.
 
 	Thank you once again for your interest and support!
 
@@ -23,7 +24,8 @@ def new_subscription(email):
 
 	# Send notification email to the admin
 	admin_msg = Message('New Subscription Alert', sender=(Config.MAIL_NAME, Config.MAIL_USERNAME), recipients=Config.ALERT_EMAILS)
-	admin_msg.body = f'''Hi,
+	admin_msg.body = f'''
+	Hi,
 
 	A new user has subscribed with the email address: {email}
 

ceb8a29ed3cf0dfaf79f83c0a278e25cebd33fbd
Tejas Satish Navalkhe <navalkhetejas18@gmail.com>
2025-01-18 15:14:00 +0530

    email code added but not using

--- tejasnavalkhe/config.py
+++ tejasnavalkhe/config.py
@@ -2,3 +2,13 @@ class Config:
 	APP_ID = '1e33b50474'
 	APP_SECRET = '55892cd316b1b30fd501'
 	SECRET_KEY = '7ba6c7fa94513272c09bc5f8c6cf664fbb85b2cc77d2a7f1593ee10a4448b2ae'
+	ALERT_EMAILS = ['navalkhetejas18@gmail.com', 'contact@tejasnavalkhe.com']
+
+	# Mail Settings
+	MAIL_SERVER = 'smtp.zoho.in'
+	MAIL_PORT = 465
+	MAIL_USE_SSL = True
+	MAIL_NAME = 'Tejas Satish Navalkhe'
+	MAIL_USERNAME = 'no_reply@tejasnavalkhe.com'
+	MAIL_PASSWORD = '5Mhh$win'
+	MAIL_SUPPRESS_SEND = False
--- tejasnavalkhe/main/views.py
+++ tejasnavalkhe/main/views.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, flash, request, render_template
-# from tejasnavalkhe.main.utils import new_subscription, is_valid_email
+from tejasnavalkhe.main.utils import new_subscription
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-	# email = request.form['email']
-	# if email and is_valid_email(email):
-	# 	new_subscription(email)
-	# 	flash('Subscription successful! A confirmation email has been sent.', 'success')
-	# else:
-	# 	flash('Please enter a valid email address.', 'danger')
+	if request.method == 'POST':
+		email = request.form.get('email')
+		new_subscription(email)
+		flash('Subscription successful! A confirmation email has been sent.', 'success')
 	return render_template('index.html')
